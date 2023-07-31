7bc6d386375007cb148961a0600e825a0e8e7bcc
Maciej Palubski <palubskimaciej@gmail.com>
2023-07-31 20:38:40 +0200

    Buttons component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import "./App.css";
 import Form from "./Form";
+import Buttons from "./Buttons";
+
+const tasks = [
+  { id: 1, content: "Przerobić listę na React", done: false },
+  { id: 1, content: "Wrócić z pracy", done: true },
+];
+
+const hideDoneTasks = false;
 
 function App() {
   return (
     <div>
       <header>
-        <h1 class="header">Lista zadań</h1>
+        <h1 className="header">Lista zadań</h1>
       </header>
 
       <main>
-        <section class="section">
-          <h2 class="section__header">Dodaj nowe zadanie</h2>
+        <section className="section">
+          <h2 className="section__header">Dodaj nowe zadanie</h2>
         </section>
         <Form />
-        <section class=" section">
-          <div class="section__container">
-            <h2 class="section__header section__header--left">Lista zadań</h2>
-            <span class="js-buttons"></span>
+        <section className=" section">
+          <div className="section__container">
+            <h2 className="section__header section__header--left">
+              Lista zadań
+            </h2>
+            <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
           </div>
-          <ul class="js-list list"></ul>
+          <ul className="js-list list"></ul>
         </section>
       </main>
     </div>
