065c58c05182785503f17300089191a636dd7bd7
Maciej Palubski <palubskimaciej@gmail.com>
2023-08-01 20:26:26 +0200

    Form turned into controlled component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <input
-      className="form__input"
-      autoFocus
-      placeholder="Co jest do zrobienia?"
-    />
-    <button className="form__button">Dodaj zadanie</button>
-  </form>
-);
+const Form = () => {
+  const [content, setContent] = useState("");
+  const onInputChange = ({ target }) => setContent(target.value.trim());
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onFormSubmit} className="form">
+      <input
+        value={content}
+        onChange={onInputChange}
+        className="form__input"
+        autoFocus
+        placeholder="Co jest do zrobienia?"
+      />
+      <button className="form__button">Dodaj zadanie</button>
+    </form>
+  );
+};
 
 export default Form;
